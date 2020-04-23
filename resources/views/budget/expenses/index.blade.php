a5caa3340e962aeab17bcfb1db7b2abb59943f5d
yuminoung <yuminoung.dev@gmail.com>
2020-04-23 13:30:30 +1000

    Update

--- resources/views/budget/expenses/index.blade.php
+++ resources/views/budget/expenses/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
     <x-headline :breadcrumbs="$breadcrumbs" class="mt-4">Expenses</x-headline>
     <x-budget.navigation></x-budget.navigation>
 @endsection
